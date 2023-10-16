2f5610e47d4fe23060219aa4409954e0d33bc4f4
Villainoux <villanuevaryan2317@gmail.com>
2023-10-17 00:49:27 +0800

    movement update

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+
+        transform.Translate(movementDirection * Time.deltaTime);
+    }
+}
