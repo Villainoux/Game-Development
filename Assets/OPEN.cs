5aba04498ae2ff511604c7bff643de2d42f7eb26
Villainoux <villanuevaryan2317@gmail.com>
2023-10-16 23:40:56 +0800

    Commit #1

---
+++ Assets/OPEN.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OPEN : MonoBehaviour
+{
+    public void QuitGame ()
+    {
+        Application.Quit ();
+        Debug.Log("PUTANGINA");
+    }
+}
